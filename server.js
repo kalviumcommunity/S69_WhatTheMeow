43c3aa4a021e3631fd92b688b52d60d4662d825b
Angel <ANGEL.MARY@kalvium.community>
2025-02-04 11:18:19 +0530

    connected to database

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const mongoose=require('mongoose');
+require('dotenv').config();
+app.use(express.json());
+const MONGO_URI=process.env.MONGO_URI;
+
+
+mongoose.connect(MONGO_URI).then(()=>console.log("Connected to database.")).catch((err)=>console.log('Failed: ',err));
 
 app.get('/ping', (req, res) => {
   res.send('pong!');
