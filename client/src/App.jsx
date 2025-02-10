87d148846593fcb5b33570eda1310fabf2049f5d
Angel <ANGEL.MARY@kalvium.community>
2025-02-10 14:00:29 +0530

    reactapp

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+// client/src/App.jsx
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="hero">
+        <h1>🐾WhatTheMeow</h1>
+        <p>Weird Cats. Punny captions. Endless smiles.</p>
+        <button>Explore Gallery</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
